001599e618538c4d70433ca9074eb02729f7ffd5
edgar <edgarabramian91@gmail.com>
2023-05-30 02:36:33 +0400

    Commit

--- app/Disc_BOT/Disc_BOT.py
+++ app/Disc_BOT/Disc_BOT.py
@@ -3,42 +3,57 @@ import os
 import interactions
 from interactions import slash_command, SlashContext, slash_option, OptionType, Button,ActionRow, ButtonStyle, listen
 from interactions.api.events import Component
-from app.CoinlFlpper.CoinFlipper import flip
-load_dotenv()
-discord_token = os.getenv('DISCORD_TOKEN')
 import random
 
+
+load_dotenv()
+
+discord_token = os.getenv('DISCORD_TOKEN')
+
+'''         probability
+   if the user has chosen heads then the 
+   probability of getting user's coin will be COEFF
+   probability of getting the opposite is equal 1-COEFF
+   COEFF sets the probability
+   flip makes a decision('H' or 'T') based on COEFF
+        '''
 COEFF = 0.2
 
 bot = interactions.Client()
 
-globals()
 bt_H = Button(
         custom_id='head',
         style=ButtonStyle.GREEN,
         label="Head",
-        disabled= False
     )
-globals()
 bt_T = Button(
         custom_id='tail',
         style=ButtonStyle.GREEN,
         label="Tail",
     )
-@slash_command(name="my_command", description="My first command :)")
+
+
+@slash_command(name="profile", description="User's Info")
 @slash_option(
     name="integer_option",
     description="Integer Option",
     required=True,
     opt_type=OptionType.INTEGER,
-
 )
 
-async def flip(ctx: SlashContext, integer_option: int):
-
-
+async def profile(ctx: SlashContext, integer_option: int):
     components: list[ActionRow] = [ActionRow(bt_T,bt_H)]
+    await ctx.send(components=components)
 
+@slash_command(name="flip", description="CoinFlipper)")
+@slash_option(
+    name="integer_option",
+    description="Integer Option",
+    required=True,
+    opt_type=OptionType.INTEGER,
+)
+async def flip(ctx: SlashContext, integer_option: int):
+    components: list[ActionRow] = [ActionRow(bt_T,bt_H)]
     await ctx.send(components=components)
 
 @listen()
@@ -48,36 +63,37 @@ async def on_component(event: Component):
             description="You Win",
             color=0x2C7F1A,
         )
-        win_embeds.set_image(
-            url="attachment://img/big-win-gambling-games-banner-with-win-vector-17338142.webp")
+
         lose_embeds = interactions.Embed(
             title="LOSE",
             description="you lose",
             color=0xFF5733)
-        # lose_embeds.set_image(
-        #     url='game-over-games-screen-glitch-computer-video-vector-22579464.webp')
 
+        win_embeds.set_image(
+            url="https://cdn5.vectorstock.com/i/1000x1000/20/74/you-win-poster-with-prize-cup-vector-17052074.jpg")
+        lose_embeds.set_image(
+            url="https://cdn5.vectorstock.com/i/1000x1000/16/84/you-lose-game-screen-slot-machine-lottery-concept-vector-44381684.jpg")
         ctx = event.ctx
 
         match ctx.custom_id:
             case "tail":
-                bt_H.disabled = True
-                user_coin='tail'#'head' if random.random() <( lambda user_coin :  0.2 if(user_coin == 'head')  else 0.8)(user_coin = user_coin) else 'tail'
-                if ('head' if random.random() < (lambda user_coin: 0.2 if (user_coin == 'head') else 0.8)(
+                user_coin='tail'
+                if ('head' if random.random() < (lambda user_coin: COEFF if (user_coin == 'head') else 1-COEFF)(
                         user_coin=user_coin) else 'tail') == user_coin:
                     await ctx.send(embeds=win_embeds)
+                    await ctx.message.delete()
                 else:
-
-                    await ctx.send(embeds=lose_embeds)
+                    await ctx.send(embed=lose_embeds)
+                    await ctx.message.delete()
             case "head":
-                bt_H.disabled = True
                 user_coin = 'head'
-                if ('head' if random.random() < (lambda user_coin: 0.2 if (user_coin == 'head') else 0.8)(
+                if ('head' if random.random() < (lambda user_coin: COEFF if (user_coin == 'head') else 1-COEFF)(
                         user_coin=user_coin) else 'tail') ==user_coin:
                     await ctx.send(embeds=win_embeds)
+                    await ctx.message.delete()
                 else:
                     await ctx.send(embeds=lose_embeds)
-
+                    await ctx.message.delete()
 
 
 bot.start(discord_token)
